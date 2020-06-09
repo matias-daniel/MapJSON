3a7d739133379903537864ad69052de467ef1e18
daniel <danielelcapo0000@gmail.com>
2020-06-09 11:06:08 -0300

    Arregle un poco el codigo :3, ahora solo faltan los objetos.

--- js/App.js
+++ js/App.js
@@ -1,19 +1,23 @@
 /// Aplicación
 
-var map = new LoadMapJSON("../res/maps/Map-prueba-00.json");
-//Ajax.loadJson("../res/maps/Map-prueba-00.json", function(data){ console.log(data);});
+function objTilesPalette(type, x, y, tam, path, data) {
+    var dic = {
+        "Walls" : new WallNoSolid(x*tam, y*tam, 16, 16, 0),
+        "Floors": new WallSolid(x*tam, y*tam, 16, 16, 0)
+    }
+    
+    return dic[type];
+}
+var map = new LoadMapJSON("../res/maps/Map-prueba-00.json", objTilesPalette);
 
 function init() {
     console.log("Aplicacion Iniciada");
-    Canvas.init(500, 500, '#33DD33');
+    Canvas.init(500, 500, '#434343');
     Canvas.setImageSmoothing(false);
 
-    for (let layer of map.objectsWallLayers) {
-        for (let o of layer.GetObjects()) {
-            obj.push(o);
-        }
-    }
-    
+    for (let o of map.objectsOfTiles)
+        obj.push(o);
+        
     mainLoop();
 }
 
@@ -29,22 +33,6 @@ function update() {
 
 function draw() {
     Canvas.clear();
-
-    // Draw TileSet
-
-    /*for (var y = 0; y < map.columns; y++) {
-        for (var x = 0; x < map.rows; x++) {
-            for (var z = 0; z < map.layers.length; z++) {
-                if (map.layers[z][x + y * map.rows] <= 0) break;
-                var sprite = map.palette.getSpriteI(map.layers[z][x + y * map.rows]-1);
-                sprite.setSize(32, 32);
-                sprite.setPosition(x*32, y*32);
-                sprite.draw();//map.dataLayer00[x + y * map.rows]
-            }
-        } 
-    }*/
-    
-    for (let i = 0; i < obj.length; i++) {
-         obj[i].draw();
-    }
+    for (let o of obj)
+        o.draw();
 }
--- js/LoadMap.js
+++ js/LoadMap.js
@@ -1,50 +1,4 @@
-class ObjectsWallLayer {
-    constructor(type, solid, data, rows, columns) {
-        this.type = type;
-        this.solid = solid;
-        this.data = data;
-        this.rows = rows;
-        this.columns = columns;
-    }
-
-    GetObjects() {
-        var objects = new Array();
-        var id = 0;
-       // console.log(this.solid);
-        switch(this.type) {
-            case "Walls":
-                switch(this.solid) {
-                    case true:
-                        for (var y = 0; y < this.columns; y++ ) {
-                            for (var x = 0; x < this.rows; x++) { 
-                                if (this.data[x+y*this.rows] <= 0) continue;
-                                objects.push(new WallSolid(x * tam, y * tam, tam, tam, id));
-                                id ++; 
-                            }
-                        }
-                        break;
-                    case false:
-                        for (var y = 0; y < this.columns; y++ ) {
-                            for (var x = 0; x < this.rows; x++) {
-                                if (this.data[x+y*this.rows] <= 0) continue;
-                                objects.push(new WallNoSolid(x * tam, y * tam, tam, tam, id));
-                                id ++; 
-                            }
-                        }
-                        break;
-                    default:
-                        console.log("Pon entre comillas el true y false");
-                }
-                break;
-            default:
-                console.log("Este tipo no esta verificado");
-        }
-        //console.log(objects);
-        return objects;
-    }
-}
-
-var LoadMapJSON = function(pathToMapJson) {
+var LoadMapJSON = function(pathToMapJson, objTilesPal) {
     this.rows = null;
     this.columns = null;
     this.tilesWidth = null;
@@ -52,11 +6,11 @@ var LoadMapJSON = function(pathToMapJson) {
     this.width = null;
     this.height = null;
     this.palette = null;
-    this.layers = new Array();
-    this.objectsWallLayers = new Array();
-    //this.path = pathToMapJson;
+    this.dataLayers = new Array();
+    this.objectsOfTiles = new Array();
+    this.p = pathToMapJson;
     var that = this;
-    //Ajax;
+
     Ajax.loadJson(pathToMapJson, function(objJson){
         that.rows = objJson.width;
         that.columns = objJson.height;
@@ -67,29 +21,16 @@ var LoadMapJSON = function(pathToMapJson) {
         that.width = that.rows * that.tilesWidth;
         that.height = that.columns * that.tilesHeight;
 
-        // Layers and layer objects
-        for (let i = 0; i < (objJson.layers.length); i++) {
-            switch(objJson.layers[i].type){
-                case "tilelayer":
-                    that.layers[i] = objJson.layers[i].data; // layers
-                    // Walls
-                    let solid = false, type = 'pete';
-                    for (let p = 0; p < objJson.layers[i].properties.length; p++) {
-                        switch(objJson.layers[i].properties[p].name) {
-                            case "Solid": solid = objJson.layers[i].properties[p].value; break;
-                            case "Type": type = objJson.layers[i].properties[p].value; console.log("._.XD");break;
-                        }
-                    }
-
-                    that.objectsWallLayers.push(new ObjectsWallLayer(type, solid, objJson.layers[i].data, that.rows, that.columns));
-
-                    break;
-                case "objectgroup":
-                    //hat.objectsLayers[i] = objJson.layers[i].objects; // layers
-                    break;
+        for (let layer of objJson.layers) {
+            if (layer.type != "tilelayer") continue;
+            for (let i = 0; i < layer.data.length; i++) {
+                that.dataLayers.push(layer.data[i]);
             }
         }
-    
+
+        // Layers and layer objects
+        that.objectsOfTiles = that.getObjectsFromTiles(that.rows, that.columns,  objJson.layers, objTilesPal, tam, pathToMapJson);
+
         // Set palette
         Ajax.loadJson("../" + objJson.tilesets[0].source, function(objJsonSource) {
             var rowsSource = objJsonSource.imagewidth / objJsonSource.tilewidth;
@@ -102,12 +43,24 @@ var LoadMapJSON = function(pathToMapJson) {
     });
 }
 
-LoadMapJSON.prototype.GetObjects = function() {
+LoadMapJSON.prototype.getObjectsFromTiles = function(r, c, layers, objTilesPal, tam, path) {
 
-}
+    var objectsFromTiles = new Array();
 
-
-/*
-    Ajax.loadJson("../" + objJson.tilesets[0].source, function(objJsonSource) {
+    for (let layer of layers) {
+        if (layer.type  != "tilelayer") continue;
         
-});*/
+        var typeProp
+        if (layer.properties.length != 0) typeProp = layer.properties[0].value;
+
+        for (let y = 0; y < c; y++) {
+            for (let x = 0; x < r; x++) {
+                //console.log(data);
+                if (layer.data[x + y * r] <= 0) continue;
+                objectsFromTiles.push(objTilesPal(typeProp, x, y, tam, path, 0));
+            }
+        }
+    }
+
+    return objectsFromTiles;
+}
